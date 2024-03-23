c6e7d90c065b824e7430eb7dc007c8e264ea1095
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-03-23 15:54:32 +0800

    Add search bar and navigation links

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { VscAccount, VscBook } from "react-icons/vsc";
-import { IoMdSearch } from "react-icons/io";
+import { IoSearchSharp } from "react-icons/io5";
 
 export default function Navigation() {
   return (
@@ -10,11 +10,11 @@ export default function Navigation() {
         <div className="flex items-center mb-5 lg:mb-0">
           <VscBook className="mr-5 icon" />
           <div className="relative flex items-center">
-            <IoMdSearch className="absolute left-0 ml-2 text-search" />
+            <IoSearchSharp className="absolute right-0 mr-2 text-search icon-search" />
             <input
               type="text"
               placeholder="I'm looking for..."
-              className="pl-8 pr-2 py-1 text-sm text-search rounded-md border border-gray-100 lg:w-80"
+              className="pl-2 pr-2 py-2 text-sm text-search rounded-lg border border-gray-100 lg:w-80"
             />
           </div>
         </div>
