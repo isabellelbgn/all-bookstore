2974c8b05fd2b1d39b4bdbc964983e4a1a68db0b
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-04-07 22:52:11 +0800

    feat: Fetch products by category

--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -1,25 +1,59 @@
 import { AddToCartButton } from "../components/AddToCartButton";
+import { TagButton } from "../components/TagButton";
+
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Book() {
+  const baseUrl = "http://127.0.0.1:8000/api";
+  const [bookData, setBookData] = useState([]);
+  const [bookImages, setBookImages] = useState([]);
+  const { book_id } = useParams();
+
+  useEffect(() => {
+    fetchData(baseUrl + `/book/` + book_id);
+  }, []);
+
+  function fetchData(baseUrl) {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setBookData(data);
+        setBookImages(data.book_images);
+      });
+  }
+
   return (
     <div className="container mx-auto px-4">
-      <main className="mt-4">
+      <main className="mt-10">
         <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-4">
-            <h1> Book Cover </h1>
+          <div className="col-span-3">
+            {bookImages.map((image, index) => (
+              <img
+                key={index}
+                src={image.image}
+                className="img-thumbnail fixed-image"
+                alt={`Book Image ${index + 1}`}
+              />
+            ))}
           </div>
-          <div className="col-span-8">
-            <h3 className="text-2xl font-bold mb-2">Book Title</h3>
-            <p className="text-gray-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse facilisis sapien vel turpis sollicitudin, nec volutpat
-              mauris aliquam. Morbi sit amet pretium risus. Proin viverra a leo
-              quis vestibulum. Aenean et diam in enim varius tincidunt a vel
-              lectus. Mauris nec ultrices erat, a tincidunt urna. Proin iaculis
-              purus mauris, vitae lacinia enim condimentum sollicitudin.
-            </p>
-            <p className="text-zinc-800 text-base font-semibold">Price</p>
-            <AddToCartButton />
+          <div className="col-span-8 flex flex-col justify-between">
+            <div>
+              <h3 className="text-2xl font-bold mb-3">{bookData.title}</h3>
+              <p className="text-gray-700 mb-3">{bookData.description}</p>
+              <p className="text-zinc-800 text-base font-semibold mb-3">
+                Price: ₱{bookData.price}
+              </p>
+              <AddToCartButton />
+            </div>
+            <div className="mt-3 mb-3">
+              <h5 className="text-zinc-800 text-sm font-semibold">Tags</h5>
+              <div className="flex">
+                <TagButton />
+                <TagButton />
+                <TagButton />
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- frontend/src/pages/CategoryBooks.jsx
+++ frontend/src/pages/CategoryBooks.jsx
@@ -1,44 +1,59 @@
+import { Link, useParams } from "react-router-dom";
 import { BookContainer } from "../components/BookContainer";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
 function CategoryBooks() {
+  const baseUrl = "http://127.0.0.1:8000/api";
+  const [categoryTitle, setCategoryTitle] = useState("");
   const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { category_id } = useParams();
 
-  const fetchBooks = useCallback((baseUrl) => {
-    fetch(baseUrl)
+  useEffect(() => {
+    fetchCategory(baseUrl + `/category/${category_id}`);
+    fetchBooks(baseUrl + `/books/?category=${category_id}`);
+  }, [category_id]);
+
+  function fetchCategory(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCategoryTitle(data.title);
+      })
+      .catch((error) => {
+        console.error("Error fetching category:", error);
+      });
+  }
+
+  function fetchBooks(url) {
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setBooks(data.results);
-        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching books:", error);
-        setLoading(false);
       });
-  }, []);
+  }
 
-  useEffect(() => {
-    fetchBooks("http://127.0.0.1:8000/api/books/");
-  }, [fetchBooks]);
+  const bookContainers = books.map((book) => (
+    <div key={book.id} className="col-span-1">
+      <BookContainer book={book} />
+    </div>
+  ));
 
   return (
     <div className="container mx-auto px-1">
       <main className="mt-4">
         <h1 className="text-xl font-medium flex justify-between items-center">
-          Category Books
+          {categoryTitle ? categoryTitle : "Category Title"}
         </h1>
-        {loading ? (
-          <p>Loading books...</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
-            {books.map((book) => (
-              <div key={book.id} className="col-span-1">
-                <BookContainer book={book} />
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+          {bookContainers.length ? (
+            bookContainers
+          ) : (
+            <p>No books found in this category.</p>
+          )}
+        </div>
       </main>
     </div>
   );
