f771d794aff999da4d5067daf111f9b71fb56e20
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-04-12 00:19:07 +0800

    Refactor: Improved structure and variable naming

--- frontend/src/pages/Customer/Login.jsx
+++ frontend/src/pages/Customer/Login.jsx
@@ -11,47 +11,6 @@ import GreenNav from "../../components/GreenNav";
 import axios from "axios";
 
 const Login = () => {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [error, setError] = useState("");
-
-  // const submit = async (e) => {
-  //   e.preventDefault();
-
-  //   if (!email || !password) {
-  //     setError("Please fill in all fields.");
-  //     return;
-  //   }
-
-  //   try {
-  //     const response = await fetch("http://localhost:8000/api/login", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       credentials: "include",
-  //       body: JSON.stringify({
-  //         email,
-  //         password,
-  //       }),
-  //     });
-
-  //     if (response.ok) {
-  //       setError("");
-  //       toast.success("Login Successful!");
-  //     } else {
-  //       const errorMessage = await response.text();
-  //       setError(
-  //         errorMessage || "Invalid email or password. Please try again."
-  //       );
-  //       toast.error(
-  //         errorMessage || "Invalid email or password. Please try again."
-  //       );
-  //     }
-  //   } catch (error) {
-  //     setError("An error occurred. Please try again later.");
-  //     console.error("Login error:", error);
-  //   }
-  // };
-
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const baseUrl = "http://127.0.0.1:8000/api/";
@@ -61,58 +20,52 @@ const Login = () => {
     password: "",
   });
 
-  const inputHandler = (event) => {
+  const handleInputChange = (event) => {
     setLoginFormData({
       ...loginFormData,
       [event.target.name]: event.target.value,
     });
   };
 
-  const submitHandler = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData();
     formData.append("username", loginFormData.username);
     formData.append("password", loginFormData.password);
 
-    axios
-      .post(baseUrl + "customer/login/", formData)
-      .then(function (response) {
-        if (response.data.success) {
-          setError(false);
-          setErrorMessage("");
-          localStorage.setItem("customer_username", loginFormData.username);
-          window.location.href = "/customer/dashboard/";
-        } else {
-          setError(true);
-          setErrorMessage(response.data.message || "Invalid credentials.");
-        }
-      })
-      .catch(function (error) {
+    try {
+      const response = await axios.post(baseUrl + "customer/login/", formData);
+
+      if (response.data.success) {
+        setError(false);
+        setErrorMessage("");
+        localStorage.setItem("customer_username", loginFormData.username);
+        window.location.href = "/customer/dashboard/";
+      } else {
         setError(true);
-        setErrorMessage("An error occurred. Please try again later.");
-        console.error("Login error:", error);
-      });
+        setErrorMessage(response.data.message || "Invalid credentials.");
+      }
+    } catch (error) {
+      setError(true);
+      setErrorMessage("An error occurred. Please try again later.");
+      console.error("Login error:", error);
+    }
   };
 
-  const buttonEnable =
+  const isFormReady = () =>
     loginFormData.username.trim() !== "" &&
     loginFormData.password.trim() !== "";
 
   return (
     <div>
       <Navigation />
-      <PageTemplate>
-        <GrayBox>
-          <h1 className="text-xl text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
-            Log In
-          </h1>
-          <form
-            className="font-normal space-y-4 md:space-y-6"
-            onSubmit={submitHandler}
-          >
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm text-black">
+      <main>
+        <section className="container mx-auto py-8">
+          <h1 className="text-3xl font-semibold text-center mb-8">Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="username" className="block mb-2 text-sm">
                 Username
               </label>
               <InputBox
@@ -121,14 +74,11 @@ const Login = () => {
                 id="username"
                 placeholder="Enter username"
                 value={loginFormData.username}
-                onChange={inputHandler}
+                onChange={handleInputChange}
               />
             </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm text-black"
-              >
+            <div className="mb-4">
+              <label htmlFor="password" className="block mb-2 text-sm">
                 Password
               </label>
               <InputBox
@@ -137,30 +87,25 @@ const Login = () => {
                 id="password"
                 placeholder="••••••••"
                 value={loginFormData.password}
-                onChange={inputHandler}
+                onChange={handleInputChange}
               />
             </div>
 
             {error && (
-              <div className="text-red-500 text-sm">{errorMessage}</div>
+              <div className="text-red-500 text-sm mb-4">{errorMessage}</div>
             )}
 
-            <div className="flex items-center justify-between">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label htmlFor="remember" className="text-gray-400">
-                    Remember me
-                  </label>
-                </div>
+            <div className="flex items-center justify-between mb-4">
+              <div className="flex items-center">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  required=""
+                />
+                <label htmlFor="remember" className="ml-2 text-sm">
+                  Remember me
+                </label>
               </div>
               <Link
                 to="#"
@@ -170,29 +115,27 @@ const Login = () => {
               </Link>
             </div>
 
-            <PrimaryButton
-              disabled={!buttonEnable}
-              className="w-full font-medium text-sm"
-            >
+            <PrimaryButton disabled={!isFormReady()} className="w-full">
               Log in
             </PrimaryButton>
-
-            <hr className="my-12 border-dotted border-t-1 bg-gray-500" />
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-light text-gray-400 mt-6">
-                Don’t have an account yet?
-              </p>
-
-              <Link
-                to="/customer/register"
-                className="text-sm text-green-50 hover:underline mt-6"
-              >
-                Sign up
-              </Link>
-            </div>
           </form>
-        </GrayBox>
-      </PageTemplate>
+
+          <hr className="my-12 border-dotted border-t-1 bg-gray-500" />
+
+          <div className="flex items-center justify-between">
+            <p className="text-sm font-light mt-6">
+              Don’t have an account yet?
+            </p>
+
+            <Link
+              to="/customer/register"
+              className="text-sm text-green-500 hover:underline mt-6"
+            >
+              Sign up
+            </Link>
+          </div>
+        </section>
+      </main>
       <Footer />
     </div>
   );
