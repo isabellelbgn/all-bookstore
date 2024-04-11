bfe3a1ecdac2cfc4ae3efb936b6a65470794fff3
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-04-12 01:12:49 +0800

    Refactor: Remove components

--- frontend/src/pages/Customer/Login.jsx
+++ frontend/src/pages/Customer/Login.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import InputBox from "../../components/Main Components/InputBox";
 import Navigation from "../../components/Main Components/Navigation";
 import Footer from "../../components/Main Components/Footer";
 import { PrimaryButton } from "../../components/Buttons/PrimaryButton";
+import { PageTemplate } from "../../components/Main Components/PageTemplate";
+import { GrayBox } from "../../components/Main Components/GrayBox";
 import { toast } from "react-toastify";
-import { Link } from "react-router-dom";
-import { PageTemplate } from "../../components/Main Components/PageTemplate";
-import { GrayBox } from "../../components/Main Components/GrayBox";
-import GreenNav from "../../components/Main Components/GreenNav";
 import axios from "axios";
-import { PageTemplate } from "../../components/Main Components/PageTemplate";
-import { GrayBox } from "../../components/Main Components/GrayBox";
-import GreenNav from "../../components/Main Components/GreenNav";
 
 const Login = () => {
   const [error, setError] = useState(false);
