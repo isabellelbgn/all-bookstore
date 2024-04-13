fe47130a880fb95adb474f2cf310691c793919eb
Martina <martina1234angeles@gmail.com>
2024-04-13 23:23:52 +0800

    Fixed: Container sizes and image issues

--- frontend/src/components/Buttons/AddToCartButton.jsx
+++ frontend/src/components/Buttons/AddToCartButton.jsx
@@ -6,7 +6,7 @@ const AddToCartButton = () => {
     <div className="relative mt-2">
       <button
         type="submit"
-        className="flex items-center font-montserrat text-white bg-green-50 text-white hover:bg-green-70 focus:ring-70 focus:outline-none focus:ring-primary-300 font-regular rounded-full text-xs px-5 py-3 text-center"
+        className="flex font-montserrat text-white bg-green-50 font-medium  hover:bg-green-70 focus:ring-70 focus:outline-none focus:ring-primary-300 font-regular rounded-full text-xs px-5 py-3 text-center"
       >
         <span>Add to cart</span>
         <span className="ml-2">
--- frontend/src/components/Main Components/GrayBox.jsx
+++ frontend/src/components/Main Components/GrayBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const GrayBox = ({ children }) => {
   return (
-    <div className="w-full bg-gray-50 rounded-2xl md:mt-0 sm:max-w-2xl xl:p-10">
+    <div className="w-full bg-gray-50 rounded-2xl md:mt-14 sm:max-w-2xl xl:p-10">
       <div className="font-montserrat p-6 space-y-4 md:space-y-6 sm:p-8">
         {children}
       </div>
