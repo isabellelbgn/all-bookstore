d8cb84915f1bc4b174711c39c7c2d321ea8ab154
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2024-04-16 21:31:38 +0800

    Feat: Add endpoint to retrieve book reviews by book ID

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 
 
