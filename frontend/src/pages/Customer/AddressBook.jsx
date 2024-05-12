75b642bb35937a765216d024886339baa1206581
YvannnZL <yzluzano18@gmail.com>
2024-05-12 21:14:36 +0800

    Update AddressBook page

--- frontend/src/pages/Customer/AddressBook.jsx
+++ frontend/src/pages/Customer/AddressBook.jsx
@@ -13,13 +13,85 @@ const AddressBook = () => {
       <div className="grid grid-cols-3 gap-1">
       <div> <Sidebar /> </div>
         <div className=" col-span-2 ml-8">
-        <div className="text-lg font-montserrat text-green-50 mb-6"> Account Information</div>
+
+        <div className="flex justify-between mt-4 mb-4">
+          <div className="text-lg font-montserrat text-green-50 mb-6"> Address Book</div>
+            <div className="flex flex-row">
+              <PrimaryButton className= " w-48 ml-40"> Add New Address</PrimaryButton>
+            </div>
+        </div>
           <div className="flex justify-between items-center">
-            <div className="text-lg font-montserrat italic mr-56">My Information</div>
+            <div className="text-lg font-montserrat italic mr-56">Default Shipping Address</div>
             <div className="flex">
               <PrimaryButton className="w-28 ml-4">Edit</PrimaryButton>
-              <PrimaryButton className="w-48 ml-4">Change Password</PrimaryButton>
+              <PrimaryButton className="w-48 ml-4">Delete Address</PrimaryButton>
             </div>
+          </div>
+
+          <div className=' container mt-9 mb-12'>
+            <table className=' table-auto w-full'>
+                <tbody>
+                    <tr className=' bg-gray-100'>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Street </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> #8 sunrise street monteritz </td>
+                    </tr>
+                    <tr>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Barangay </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                    <tr  className=' bg-gray-100'>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'>City </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                    <tr>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Region </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> Davao del Sur </td>
+                    </tr>
+                    <tr  className=' bg-gray-100'
+                    >
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Zip Code </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div className="text-lg font-montserrat italic mr-56">Address #2</div>
+              <div className="flex">
+              <PrimaryButton className="w-28 ml-4">Edit</PrimaryButton>
+              <PrimaryButton className="w-48 ml-4">Delete Address</PrimaryButton>
+              </div>
+            </div>
+          <div className=' container mt-9 mb-12'>
+            <table className=' table-auto w-full'>
+                <tbody>
+                    <tr className=' bg-gray-100'>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Street </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> #8 sunrise street monteritz </td>
+                    </tr>
+                    <tr>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Barangay </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                    <tr  className=' bg-gray-100'>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'>City </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                    <tr>
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Region </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> Davao del Sur </td>
+                    </tr>
+                    <tr  className=' bg-gray-100'
+                    >
+                        <td className='px-4 py-6 text-left font-[montserrat] font-bold'> Zip Code </td>
+                        <td className='px-4 py-6 text-right font-[montserrat] w-2/3'> </td>
+                    </tr>
+                </tbody>
+
+            </table>
           </div>
           
         </div>
