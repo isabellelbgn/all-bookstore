331d8f532ba438b1b362ac764ed37ddc8182d271
Martina <martina1234angeles@gmail.com>
2024-05-15 17:18:48 +0800

    Fixed sidebar color  current location

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { PrimaryButton } from "./Buttons/PrimaryButton";
 import AuthContext from "../context/AuthContext";
@@ -7,6 +7,7 @@ import AuthContext from "../context/AuthContext";
 const Sidebar = () => {
   const navigate = useNavigate();
   const { logoutCustomer } = useContext(AuthContext);
+  const location = useLocation();
 
   const handleLogout = () => {
     logoutCustomer();
@@ -18,25 +19,41 @@ const Sidebar = () => {
       <div className="flex flex-col bg-gray-100 font-montserrat border-spacing-4 p-2.5 overflow-y-auto">
         <Link
           to="/customer/dashboard"
-          className="px-4 py-6 text-gray-400 border-b-2"
+          className={`px-4 py-6 ${
+            location.pathname === "/customer/dashboard"
+              ? "text-black border-green-50"
+              : "text-gray-400"
+          } border-b-2`}
         >
           Dashboard
         </Link>
         <Link
           to="/customer/dashboard/orders"
-          className="px-4 py-6 text-gray-400 border-b-2"
+          className={`px-4 py-6 ${
+            location.pathname === "/customer/dashboard/orders"
+              ? "text-black border-green-50"
+              : "text-gray-400"
+          } border-b-2`}
         >
           My Orders
         </Link>
         <Link
           to="/customer/dashboard/address"
-          className="px-4 py-6 text-gray-400 border-b-2"
+          className={`px-4 py-6 ${
+            location.pathname === "/customer/dashboard/address"
+              ? "text-black border-green-50"
+              : "text-gray-400"
+          } border-b-2`}
         >
           Address Book
         </Link>
         <Link
           to="/customer/dashboard/account"
-          className="px-4 py-6 text-gray-400 border-b-2"
+          className={`px-4 py-6 ${
+            location.pathname === "/customer/dashboard/account"
+              ? "text-black border-green-50"
+              : "text-gray-400"
+          } border-b-2`}
         >
           Account Information
         </Link>
